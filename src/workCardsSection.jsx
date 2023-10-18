1239af9502a360beea3ed42015832bfd2ffbfd55
Harsh Panchal <harsh.panchal.me@gmail.com>
2023-10-18 22:30:15 +0530

    made changes to work cards stating Cognito.dev is completed.

--- src/workCardsSection.jsx
+++ src/workCardsSection.jsx
@@ -5,8 +5,8 @@ export default function AllCards() {
   return (
     <>
       <WorkCard projectLink={'https://github.com/harshtohai/Shell-Net'} projectName={'Shell-Net'} status='Completed' bodyText={'A Terminal based Social Media platform. Giving users speed.'} tech={'Python'} />
-      <WorkCard projectLink={'https://github.com/harshtohai/Shell-Net'} projectName={'HarshToHai'} status='In Progress' bodyText={'My personal website.'} tech={'React Js'} />
-      <WorkCard projectLink={'https://github.com/harshtohai/Shell-Net'} projectName={'OG Repo'} status='On Hold' bodyText={'Providing users a new UI Interface of GitHub with feed and chat feature.'} tech={'React Js'} />
+      <WorkCard projectLink={'https://github.com/harshtohai/Cognito.dev'} projectName={'Cognito.dev'} status='Completed' bodyText={'My personal website.'} tech={'React Js'} />
+      <WorkCard projectLink={'https://github.com/harshtohai'} projectName={'OG Repo'} status='On Hold' bodyText={'Providing users a new UI Interface of GitHub with feed and chat feature.'} tech={'React Js'} />
       
     </>
   )
