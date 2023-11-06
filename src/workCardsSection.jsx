80e057352ffe34cbd59fe3f12a552a3166bbe174
Harsh Panchal <harsh.panchal.me@gmail.com>
2023-11-06 22:51:09 +0530

    Added Termtion in work card

--- src/workCardsSection.jsx
+++ src/workCardsSection.jsx
@@ -6,6 +6,7 @@ export default function AllCards() {
     <>
       <WorkCard projectLink={'https://github.com/harshtohai/Shell-Net'} projectName={'Shell-Net'} status='Completed' bodyText={'A Terminal based Social Media platform. Giving users speed.'} tech={'Python'} />
       <WorkCard projectLink={'https://github.com/harshtohai/Cognito.dev'} projectName={'Cognito.dev'} status='Completed' bodyText={'My personal website.'} tech={'React Js'} />
+      <WorkCard projectLink={'https://github.com/harshtohai/TermTion'} projectName={'TermTion'} status='Completed' bodyText={'Its a Terminal based Notion like app.'} tech={'Python'} />
       <WorkCard projectLink={'https://github.com/harshtohai'} projectName={'OG Repo'} status='On Hold' bodyText={'Providing users a new UI Interface of GitHub with feed and chat feature.'} tech={'React Js'} />
       
     </>
