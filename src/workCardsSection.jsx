b1843f7808f1d3bf8cee09b68cf77885cab612fa
Harsh Panchal <harsh.panchal.me@gmail.com>
2024-01-18 14:51:45 +0530

    Spelling mistake :smiling_face_with_tear:

--- src/workCardsSection.jsx
+++ src/workCardsSection.jsx
@@ -7,7 +7,7 @@ export default function AllCards() {
       <WorkCard projectLink={'https://github.com/harshtohai/Shell-Net'} projectName={'Shell-Net'} status='Completed' bodyText={'A Terminal based Social Media platform. Giving users speed.'} tech={'Python'} />
       <WorkCard projectLink={'https://github.com/harshtohai/Cognito.dev'} projectName={'Cognitowl.dev'} status='Completed' bodyText={'My personal website "This Website".'} tech={'React Js'} /> 
       <WorkCard projectLink={'https://github.com/harshtohai'} projectName={'OG Repo'} status='On Hold' bodyText={'Providing users a new UI Interface of GitHub with feed and chat feature.'} tech={'React Js'} />
-      <WorkCard projectLink={'https://github.com/techslangs'} projectName={'Tech Slangs'} status='In Progress' bodyText={'A dicteonary of Tech Terms made by you.'} tech={'Nextjs Prisma'} />
+      <WorkCard projectLink={'https://github.com/harshtohai/techslangs'} projectName={'Tech Slangs'} status='In Progress' bodyText={'A dictonary of Tech Terms made by you.'} tech={'Nextjs Prisma'} />
     </>
   )
 
